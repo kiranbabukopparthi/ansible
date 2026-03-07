6ad2493b8a89b6c3ddaafc319e18fd004aa6adbb
Kopparthi Kiran Babu <thekiranbabukopparthi@gmail.com>
2026-03-07 11:32:08 +0530

    ansible

--- ansible2.sh
+++ ansible2.sh
@@ -105,3 +105,44 @@ To give entire list of servers in a project
 To give dispatach server 
 we can give directly without includinh it any group
 
+To run the inventory file
+ansible frontend -i inventory.ini --list-hosts
+This command will show you the list of frontend servers which are available in inventory
+
+if we want backend servers 
+ansible backend -i inventory.ini --list-hosts
+
+if we want all servers 
+ansible all -i inventory.ini --list-hosts
+
+if we want specific group like roboshop servers 
+ansible roboshop -i inventory.ini --list-hosts
+
+if we want ungrouped or dispatch servers 
+ansible ungrouped -i inventory.ini --list-hosts
+
+To see the servers in a graph view
+ansible-inventory -i inventory.ini --graph
+
+
+Ansible is built in python
+To connect with amazon or azure etc.. via ansible
+
+we will write the ansible script in playbooks
+once we give input in playbooks, ansible runs inside python and connect to 
+the system using  API
+
+Ansible handles YAML format easily.
+
+consider a server in inventory and we are trying to ping that server using playbook
+playbook1.yaml
+- name: ping your server
+  hosts: web
+  tasks:
+  - name: ping the web server
+    ansible.builtin.ping:
+
+command to run the script
+ansible-playbook -i inventory.ini -e anisble_user=ec2-user -e ansible_password=DevOps321 playbook1.yaml
+
+
